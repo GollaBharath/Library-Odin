91dd980c459186a5879fad7dcc45a98e3d502911
Golla Bharath <gollabharath2007@gmail.com>
2025-09-16 22:05:50 +0530

    JS part complete

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ books = [];
 let cBooks = document.querySelector(".c-books");
 let pBooks = document.querySelector(".p-books");
 
-// ❌ ✅
+let newBookForm = document.querySelector(".newBookForm");
+
 function book(id, name, author, pages, read) {
 	this.id = id;
 	this.name = name;
@@ -16,6 +17,26 @@ function addNewBook(name, author, pages, read) {
 	const id = crypto.randomUUID();
 	let B1 = new book(id, name, author, pages, read);
 	books.push(B1);
+}
+
+function newBtn() {
+	newBookForm.show();
+}
+
+function newBook() {
+	bookName = document.getElementById("fname");
+	bookAuthor = document.getElementById("fauthor");
+	bookPages = document.getElementById("fpages");
+	bookRead = document.getElementById("fread");
+
+	addNewBook(
+		bookName.value,
+		bookAuthor.value,
+		bookPages.value,
+		bookRead.checked
+	);
+
+	renderBooks();
 }
 
 book.prototype["flipStatus"] = function () {
@@ -34,7 +55,8 @@ function renderBooks() {
 					<th class="name">${x.name}</th>
 					<td class="author">${x.author}</td>
 					<td class="pages">${x.pages}</td>
-					<td class="status"><button class="flip" data=${x.flipStatus}>✅</button> Completed</td>
+					<td class="status"><button class="flip" onclick="swapClicked('${x.id}')">✅</button></td>
+					<td class="del"><button onclick="delBook('${x.id}')">Delete</button></td>
 				</tr>
             `;
 		} else {
@@ -43,27 +65,38 @@ function renderBooks() {
 					<th class="name">${x.name}</th>
 					<td class="author">${x.author}</td>
 					<td class="pages">${x.pages}</td>
-					<td class="status"><button class="flip" data=${x.flipStatus}>❌</button> Pending</td>
+					<td class="status"><button class="flip" onclick="swapClicked('${x.id}')">❌</button></td>
+					<td class="del"><button onclick="delBook('${x.id}')">Delete</button></td>
 				</tr>
             `;
 		}
 	}
 }
 
-addNewBook("qwe1", "qewr", 20, true);
-addNewBook("qwe2", "qewr", 20, false);
-addNewBook("qwe3", "qewr", 20, true);
-addNewBook("qwe4", "qewr", 20, false);
+function delBook(id) {
+	let ind = 0;
+	for (x in books) {
+		if (books[x].id == id) {
+			ind = x;
+		}
+	}
+	console.log(ind);
+	books.splice(ind, 1);
+	renderBooks();
+}
+
+addNewBook("The Hobbit", "J.R.R. Tolkien", 310, true);
+addNewBook("1984", "George Orwell", 328, false);
+addNewBook("To Kill a Mockingbird", "Harper Lee", 281, true);
+addNewBook("The Catcher in the Rye", "J.D. Salinger", 277, false);
+addNewBook("Pride and Prejudice", "Jane Austen", 279, true);
 
 renderBooks();
-let flipBtns = document.querySelectorAll(".flip");
-console.log(flipBtns);
-for (btn of flipBtns) {
-	console.log(btn);
-	id = btn.getAttribute("data");
-	console.log(id);
-	btn.addEventListener("click", () => {
-		console.log(id);
-		id();
-	});
+
+function swapClicked(id) {
+	for (x in books) {
+		if (books[x].id == id) {
+			books[x].flipStatus();
+		}
+	}
 }
